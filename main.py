55ab2c19611de8de6ff193e96102ceb1cacaeee2
Wenbin Liao <65928491+set-path@users.noreply.github.com>
2025-05-20 18:14:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 import time
 
-from utils import get_daily_papers_by_keyword_with_retries, generate_table, back_up_files,\
-    restore_files, remove_backups, get_daily_date
+from utils import get_daily_papers_by_keyword_with_retries, generate_table, back_up_readme_file, get_daily_date
+
 
 # keywords
 keywords = ["Open Vocabulary Semantic Segmentation", "Remote Sensing Segmentation", "Remote Sensing Vision Language Model", "Vision Language Model"] # TODO add more keywords
@@ -13,33 +13,44 @@ issues_result = 15 # maximum papers to be included in the issue
 # all columns: Title, Link, Abstract, Date, Tags, Comment
 column_names = ["Title", "Link", "Abstract", "Date", "Comment"]
 
-back_up_files() # back up README.md
+all_papers = {}
 
-# write to README.md
-f_rm = open("README.md", "w") # file for README.md
+for keyword in keywords:
+    if len(keyword.split()) == 1:
+        link = "AND" # for keyword with only one word, We search for papers containing this keyword in both the title and abstract.
+    else:
+        link = "OR"
+    papers = get_daily_papers_by_keyword_with_retries(keyword, column_names, max_result, link)
+    if papers is not None:
+        all_papers[keyword] = papers
+all_papers.pop("Remote Sensing Vision Language Model")
+if len(all_papers) == 0:
+    print("No paper found!")
+    sys.exit("No paper found!")
+
+cur_date = get_daily_date()
+
+f_rm = open("README.md", "w", encoding="utf-8")
+f_is = open("ISSUE.md", "w", encoding="utf-8")
+
 f_rm.write("# Daily Papers\n")
 
-# write to ISSUE_TEMPLATE.md
-f_is = open(".github/ISSUE_TEMPLATE.md", "w") # file for ISSUE_TEMPLATE.md
 f_is.write("---\n")
-f_is.write("title: Latest {0} Papers - {1}\n".format(issues_result, get_daily_date()))
+f_is.write("title: Papers - {0}\n".format(cur_date))
 f_is.write("labels: documentation\n")
 f_is.write("---\n")
 
 for keyword in keywords:
-    f_rm.write("## {0}\n".format(keyword))
-    f_is.write("## {0}\n".format(keyword))
-    if len(keyword.split()) == 1: link = "AND" # for keyword with only one word, We search for papers containing this keyword in both the title and abstract.
-    else: link = "OR"
-    papers = get_daily_papers_by_keyword_with_retries(keyword, column_names, max_result, link)
-    if papers is None: # failed to get papers
-        print("Failed to get papers!")
-        f_rm.close()
-        f_is.close()
-        restore_files()
-        sys.exit("Failed to get papers!")
-    rm_table = generate_table(papers)
-    is_table = generate_table(papers[:issues_result], ignore_keys=["Abstract"])
+    if keyword not in all_papers:
+        f_rm.write("## {0} - No Paper Found\n".format(keyword))
+        f_is.write("## {0} - No Paper Found\n".format(keyword))
+        continue
+
+    f_rm.write("## {0} - {1} in Total\n".format(keyword, len(all_papers[keyword])))
+    f_is.write("## {0} - Preview {1} papers\n".format(keyword, len(all_papers[keyword][:issues_result])))
+    
+    rm_table = generate_table(all_papers[keyword])
+    is_table = generate_table(all_papers[keyword][:issues_result], ignore_keys=["Abstract"])
     f_rm.write(rm_table)
     f_rm.write("\n\n")
     f_is.write(is_table)
@@ -48,3 +59,5 @@ for keyword in keywords:
 
 f_rm.close()
 f_is.close()
+
+back_up_readme_file(cur_date)
