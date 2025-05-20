6b69cbad0ba4dfd758148cdc234c834c2db5fbd8
Wenbin Liao <65928491+set-path@users.noreply.github.com>
2025-05-20 18:14:11 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -125,20 +125,9 @@ def generate_table(papers: List[Dict[str, str]], ignore_keys: List[str] = []) ->
         body += "\n| " + " | ".join(paper.values()) + " |"
     return header + body
 
-def back_up_files():
+def back_up_readme_file(backup_date):
     # back up README.md and ISSUE_TEMPLATE.md
-    shutil.move("README.md", "README.md.bk")
-    shutil.move(".github/ISSUE_TEMPLATE.md", ".github/ISSUE_TEMPLATE.md.bk")
-
-def restore_files():
-    # restore README.md and ISSUE_TEMPLATE.md
-    shutil.move("README.md.bk", "README.md")
-    shutil.move(".github/ISSUE_TEMPLATE.md.bk", ".github/ISSUE_TEMPLATE.md")
-
-def remove_backups():
-    # remove README.md and ISSUE_TEMPLATE.md
-    os.remove("README.md.bk")
-    os.remove(".github/ISSUE_TEMPLATE.md.bk")
+    shutil.copy("README.md", f"backup/Papers - {backup_date}.md")
 
 def get_daily_date():
     # get beijing time in the format of "March 1, 2021"
